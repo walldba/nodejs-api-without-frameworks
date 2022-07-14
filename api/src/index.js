c7575f688029c0a3b6f38cfb4e689ba1ceece1fd
walldba <wallacedba@gmail.com>
2022-07-14 14:38:57 -0300

    feat: init server using http native lib

---
+++ api/src/index.js
@@ -0,0 +1,11 @@
+import http from 'node:http';
+
+const PORT = process.env.PORT || 3000;
+
+const server = http
+  .createServer((request, response) => {
+    response.end('Hello World');
+  })
+  .listen(PORT, () => console.log(`sever is running at ${PORT}`));
+
+export { server };
