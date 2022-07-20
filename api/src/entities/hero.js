94096c1730baf07d006a6ab34052546e5db5b385
walldba <wallacedba@gmail.com>
2022-07-20 18:18:14 -0300

    refactor: add destructuring in hero constructor

--- api/src/entities/hero.js
+++ api/src/entities/hero.js
@@ -1,7 +1,7 @@
 import { randomUUID } from 'node:crypto';
 
 export default class Hero {
-  constructor(name, age, power) {
+  constructor({ name, age, power }) {
     this.id = randomUUID();
     this.name = name;
     this.age = age;
